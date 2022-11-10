da8d58b060f9145b9174af2455cf378ff8c6216c
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-10 11:56:19 +0100

    books works with one any

--- src/AppContext.tsx
+++ src/AppContext.tsx
@@ -11,18 +11,42 @@ interface IAppProvider {
 	children: React.ReactNode;
 }
 
+interface IBook {
+	id: number;
+	idCode: string;
+	title: string;
+	description: string;
+	language: string;
+}
+
+function capitalizeFirstLetter(line: string) {
+  return line.charAt(0).toUpperCase() + line.slice(1);
+}
+
 export const AppContext = createContext<IAppContext | null>(null);
 
 const booksUrl = 'https://edwardtanguay.vercel.app/share/techBooks.json';
 
 export const AppProvider: React.FC<IAppProvider> = ({ children }) => {
-	const [books, setBooks] = useState([]);
+	const [books, setBooks] = useState<IBook[]>([]);
 	const appTitle = 'The Berlin Study Group';
 
 	useEffect(() => {
 		setTimeout(() => {
 			(async () => {
-				setBooks((await axios.get(booksUrl)).data);
+				const rawBooks = (await axios.get(booksUrl)).data;
+				const _books:IBook[] = [];
+				rawBooks.forEach((rawBook:any) => {
+					const _book: IBook = {
+						id: rawBook.id,
+						idCode: rawBook.idCode,
+						title: rawBook.title,
+						description: rawBook.description,
+						language: rawBook.language === '' ? 'English' : capitalizeFirstLetter(rawBook.language)
+					};
+					_books.push(_book);
+				})
+				setBooks(_books);
 			})();
 		}, 1000);
 	}, []);
